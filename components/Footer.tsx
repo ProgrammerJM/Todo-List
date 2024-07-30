dffc82723df62c2b55a898c001b059af57eb3583
John Mark Tizado <johnmarktizado@gmail.com>
2024-07-30 18:21:16 +0800

    chore: Update footer component with social media links and developer name

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,31 +4,33 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="flex flex-col md:flex-row items-center justify-center w-full h-auto md:h-24 p-4 bg-black text-white">
-      <p className="text-sm mb-2 md:mb-0">
-        Develop by John Mark Tizado with love
-      </p>
-      <p className="text-sm mx-2 hidden md:block">&bull;</p>
-      <Link
-        href={"https://github.com/ProgrammerJM"}
-        target="_blank"
-        className="text-sm mb-2 md:mb-0"
-        aria-label="Github"
-      >
-        Github
-      </Link>
-      <p className="text-sm mx-2 hidden md:block">&bull;</p>
-      <Link
-        href={"https://x.com/xyjtiz"}
-        target="_blank"
-        className="text-sm mb-2 md:mb-0"
-        aria-label="Twitter"
-      >
-        Twitter
-      </Link>
-      <p className="text-sm mx-2 hidden md:block">&bull;</p>
-      <p className="text-sm">
-        &copy; {new Date().getFullYear()} Todo App - PERN Stack
-      </p>
+      <div className="flex flex-col justify-center items-center">
+        <p className="text-sm">
+          &copy; {new Date().getFullYear()} Todo App - PERN Stack
+        </p>
+        <div className="flex items-center justify-center mt-5">
+          <Link
+            href={"https://github.com/ProgrammerJM"}
+            target="_blank"
+            className="text-sm mb-2 md:mb-0 mr-2"
+            aria-label="Github"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-linkedin h-5 w-5"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+            </svg>
+          </Link>
+          <p className="text-sm mb-2 md:mb-0">
+            Develop by John Mark Tizado with {"<3"}
+          </p>
+        </div>
+      </div>
     </footer>
   );
 };
