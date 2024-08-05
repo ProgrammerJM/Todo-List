0d40230dc77b8a2703c5c8e47217865d555d41ec
John Mark Tizado <johnmarktizado@gmail.com>
2024-08-05 16:01:11 +0800

    chore: Add loading state to ListTodo component

--- components/ListTodo.tsx
+++ components/ListTodo.tsx
@@ -5,6 +5,7 @@ import EditTodo from "@/components/EditTodo";
 
 export default function ListTodo() {
   const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const getTodos = async () => {
     try {
@@ -12,8 +13,10 @@ export default function ListTodo() {
       const jsonData = await response.json();
 
       setTodos(jsonData);
+      setLoading(false);
     } catch (err: any) {
       console.error(err.message);
+      setLoading(false);
     }
   };
 
@@ -35,6 +38,14 @@ export default function ListTodo() {
     getTodos();
   }, []);
 
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-96">
+        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+      </div>
+    );
+  }
+
   return (
     <div className="overflow-x-auto p-4 sm:p-8">
       <table className="mx-auto w-full max-w-4xl border-collapse">
@@ -52,35 +63,27 @@ export default function ListTodo() {
           </tr>
         </thead>
         <tbody>
-          {todos &&
-            todos.map((todo: { todo_id: number; description: string }) => (
-              <tr
-                key={todo.todo_id}
-                className="dark:odd:bg-white dark:even:bg-gray-100 odd:bg-black even:bg-gray-800"
-              >
-                <td className="px-4 py-2 border-b border-gray-300 dark:text-black text-white">
-                  {todo.description}
-                </td>
-                <td className="px-4 py-2 border-b border-gray-300">
-                  <EditTodo todo={todo} />
-                </td>
-                <td className="px-4 py-2 border-b border-gray-300">
-                  <button
-                    className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700 transition-colors duration-300"
-                    onClick={() => deleteTodo(todo.todo_id)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}{" "}
-          : (
-          <tr>
-            <td colSpan={3} className="text-center py-4">
-              Fetching Data... Please Wait
-            </td>
-          </tr>
-          )
+          {todos.map((todo: { todo_id: number; description: string }) => (
+            <tr
+              key={todo.todo_id}
+              className="dark:odd:bg-white dark:even:bg-gray-100 odd:bg-black even:bg-gray-800"
+            >
+              <td className="px-4 py-2 border-b border-gray-300 dark:text-black text-white">
+                {todo.description}
+              </td>
+              <td className="px-4 py-2 border-b border-gray-300">
+                <EditTodo todo={todo} />
+              </td>
+              <td className="px-4 py-2 border-b border-gray-300">
+                <button
+                  className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-700 transition-colors duration-300"
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
